f1afe82b8bcbc5e97b5347c22fa1dbc0db087fb9
Nimitpamnani <C0764944@mylambton.ca>
2020-06-13 00:03:57 -0400

    withCustomComonenets

--- src/Customize.js
+++ src/Customize.js
@@ -7,6 +7,15 @@ render(){
 return  <div>     
 <Button  type = "/"  name=" goBack"  > </Button>
 <h1>Customize your own meme  </h1>
+
+<Button   name=" Browse to upload image"  > </Button>
+<br />
+<input type ="text" placeholder=" uppertext"  ></input>
+<br />
+<input type ="text" placeholder=" lower text"  ></input>
+
+<canvas></canvas>
+
 </div>  
 
 }
